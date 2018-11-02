3c9f6967904c7a3acb1ef4d1cbc0ea78485ea116
Vincent L <liberty.sky87@gmail.com>
2018-11-01 21:46:53 -0700

    ALL WORKING, fixed style issues, pagination, conditional button render

--- client/src/components/ReviewsNavi.jsx
+++ client/src/components/ReviewsNavi.jsx
@@ -9,7 +9,7 @@ const ReviewsNavi =({pgs, activePg, changePage})=> (
         <button type="button" className="navi-page-link" onClick={(e)=> changePage(activePg-1)} >
           <div className="navi-next-container" >
             <div className="navi-next-arrow">
-              <svg viewBox="0 0 18 18" role="img" aria-label="Previous" focusable="false"><path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd"></path></svg>
+              <svg className ="navi-arrow" viewBox="0 0 18 18" role="img" aria-label="Previous" focusable="false"><path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd"></path></svg>
               </div>
           </div>
         </button>
@@ -21,7 +21,7 @@ const ReviewsNavi =({pgs, activePg, changePage})=> (
         <button type="button" className="navi-page-link" onClick={(e)=>changePage(activePg+1)}>
           <div className="navi-next-container">
             <div className="navi-next-arrow">
-              <svg viewBox="0 0 18 18" role="img" aria-label="Next" focusable="false"><path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd"></path></svg>
+              <svg className ="navi-arrow" viewBox="0 0 18 18" role="img" aria-label="Next" focusable="false"><path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd"></path></svg>
               </div>
           </div>
         </button>

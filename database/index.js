7b67739fe116e9683b28705113f8140177840a8d
Vincent L <liberty.sky87@gmail.com>
2018-11-09 18:39:29 -0800

    test hardcode uri

--- database/index.js
+++ database/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 //const mongoUri = 'mongodb://localhost/Reviews';
 
-const db = mongoose.connect(process.env.MONGOURI, { useNewUrlParser: true });
+const db = mongoose.connect('mongodb://admin:heathcliff0@ds155903.mlab.com:55903/airbnb-reviews', { useNewUrlParser: true });
 
 module.exports = db;

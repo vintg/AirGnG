b765577d9454a8717a2ff273d451ff0b241fa793
Vincent L <liberty.sky87@gmail.com>
2018-11-10 16:03:31 -0800

    aws setup

--- database/index.js
+++ database/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://admin:heathcliff0@ds155903.mlab.com:55903/airbnb-reviews';
 //'mongodb://localhost/Reviews';
-const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
+const db = mongoose.connect('mongodb://admin:heathcliff0@ds155903.mlab.com:55903/airbnb-reviews', { useNewUrlParser: true });
 
 module.exports = db;
